d62694a27cc35cd2026564d3db1c6533dfbbc6f0
rabby4 <mdrabbybd4747@gmail.com>
2023-10-18 15:33:20 +0600

    design login and registration page

--- src/components/pages/Login/Login.jsx
+++ src/components/pages/Login/Login.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
+import { FcGoogle } from 'react-icons/fc';
+import { BsGithub } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
+
 
 const Login = () => {
     return (
         <div>
-            <h2>this is login page</h2>
+            <div className="max-w-7xl h-[700px] flex items-center mx-auto lg:px-0 md:px-0 px-5">
+
+                <div className="card flex-shrink-0 w-full max-w-lg shadow-xl mx-auto">
+                    <div className='mt-10 mb-5'>
+                        <h2 className='text-center text-4xl font-semibold'>Please Login</h2>
+                    </div>
+                    <form className="card-body">
+                        <div className="form-control mb-3">
+                            <label className="label">
+                                <span className="label-text font-medium">Email</span>
+                            </label>
+                            <input type="email" placeholder="Email" name='email' className="input input-bordered" required />
+                        </div>
+                        <div className="form-control mb-3">
+                            <label className="label">
+                                <span className="label-text font-medium">Password</span>
+                            </label>
+                            <input type="password" placeholder="Password" name='password' className="input input-bordered" required />
+
+                        </div>
+                        <div className="form-control">
+                            <button className="btn bg-orange-600 hover:bg-orange-500 text-white text-base">Login</button>
+                        </div>
+                    </form>
+                    <div>
+                        <p className='text-sm text-center mb-5'>New in this website? Please <Link to='/register'><strong className='text-orange-600'>Register</strong></Link></p>
+                    </div>
+                    <div className="px-10 mb-10">
+                        <button className='flex items-center w-full py-2 mt-3 border-2 border-gray-700 justify-center gap-2 rounded-md hover:text-orange-600 hover:border-orange-600 duration-300'>
+                            <FcGoogle></FcGoogle>
+                            <span>Login with Google</span>
+                        </button>
+                        <button className='flex items-center w-full py-2 mt-3 border-2 border-gray-700 justify-center gap-2 rounded-md hover:text-orange-600 hover:border-orange-600 duration-300'>
+                            <BsGithub></BsGithub>
+                            <span>Login with Github</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/pages/Register/Register.jsx
+++ src/components/pages/Register/Register.jsx
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { IoIosEyeOff, IoIosEye } from 'react-icons/io';
+import { Link } from 'react-router-dom';
+
 
 const Register = () => {
+    const [showPassword, setShowPassword] = useState(false)
+
     return (
         <div>
-            <h2>This is register page</h2>
+            <div className="max-w-7xl h-screen flex items-center mx-auto lg:px-0 md:px-0 px-5">
+                <div className="card flex-shrink-0 w-full max-w-lg shadow-xl mx-auto">
+                    <div className='mt-10 mb-5'>
+                        <h2 className='text-center text-4xl font-semibold'>Register Now</h2>
+                    </div>
+                    <form className="card-body">
+                        <div className="form-control mb-3">
+                            <label className="label">
+                                <span className="label-text font-medium">Email</span>
+                            </label>
+                            <input type="email" placeholder="Email" name='email' className="input input-bordered" required />
+                        </div>
+                        <div className="form-control mb-3">
+                            <label className="label">
+                                <span className="label-text font-medium">Name</span>
+                            </label>
+                            <input type="text" placeholder="Your Name" name='name' className="input input-bordered" required />
+                        </div>
+                        <div className="form-control mb-3">
+                            <label className="label">
+                                <span className="label-text font-medium">Photo URL</span>
+                            </label>
+                            <input type="text" placeholder="Photo URL" name='photo' className="input input-bordered" required />
+                        </div>
+                        <div className="form-control mb-3">
+                            <label className="label">
+                                <span className="label-text font-medium">Password</span>
+                            </label>
+                            <div className="relative">
+                                <input type={showPassword ? 'text' : 'password'} placeholder="Password" name='password' className="input input-bordered relative w-full" required />
+                                <span className="cursor-pointer absolute right-3 bottom-4 text-xl" onClick={() => setShowPassword(!showPassword)}>
+                                    {
+                                        showPassword ? <IoIosEyeOff></IoIosEyeOff> : <IoIosEye></IoIosEye>
+                                    }
+                                </span>
+                            </div>
+
+                        </div>
+                        <div className="form-control">
+                            <button className="btn bg-orange-600 hover:bg-orange-500 text-white text-base">Register</button>
+                        </div>
+                    </form>
+                    <div className='mb-5'>
+                        <p className='text-sm text-center mb-5'>Do not have an account? Please <Link to='/login'><strong className='text-orange-500'>Login</strong></Link></p>
+                    </div>
+
+                </div>
+                {/* <Toaster></Toaster> */}
+            </div>
         </div>
     );
 };
